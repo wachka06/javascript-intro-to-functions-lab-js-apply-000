908b942d27ae62a7ec6393ee8ccdc543ed2327fb
wachka06 <wachka06@gmail.com>
2018-04-02 22:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
   }
-}
-
-function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  }  
+  }    
 }
