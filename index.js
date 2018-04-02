976275959eac643a21e33701e9b3116393a9ecf9
wachka06 <wachka06@gmail.com>
2018-04-02 23:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,8 @@ function sayHiToGrandma(string) {
   }
   if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  }    
+  }   
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  }
 }
